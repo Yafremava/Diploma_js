6a3a944f0ed5bacbf19ecb98c0a92bcd98947524
Efremova Ekaterina <kayfremava@yandex.by>
2020-04-03 04:27:43 +0100

    diploma

--- script/script.js
+++ script/script.js
@@ -2,13 +2,15 @@ window.addEventListener('DOMContentLoaded', () => {
   'use strict';
   //popup-call
   const headerPopupCall = () =>{
-    const callBtn = document.querySelector('.call-btn'),
+    const callBtn = document.querySelectorAll('.call-btn'),
       popupCall = document.querySelector('.popup-call');
-
-    callBtn.addEventListener('click', () =>{
-      popupCall.style.display = 'block';
+    
+    callBtn.forEach((elem) =>{
+      elem.addEventListener('click', () =>{
+        popupCall.style.display = 'block';
+      }); 
     });
-
+   
     popupCall.addEventListener('click', () =>{
       let target = event.target;
       if(target.classList.contains('popup-close')){
