9fdd91fc4bda1d2691b23ecf478b0cebcecfaaaf
Efremova Ekaterina <kayfremava@yandex.by>
2020-04-03 04:15:00 +0100

    diploma

--- script/script.js
+++ script/script.js
@@ -4,9 +4,45 @@ window.addEventListener('DOMContentLoaded', () => {
   const headerPopupCall = () =>{
     const callBtn = document.querySelector('.call-btn'),
       popupCall = document.querySelector('.popup-call');
+
     callBtn.addEventListener('click', () =>{
       popupCall.style.display = 'block';
     });
+
+    popupCall.addEventListener('click', () =>{
+      let target = event.target;
+      if(target.classList.contains('popup-close')){
+        popupCall.style.display = 'none';
+      } else {
+        target = target.closest('.popup-content');
+        if(!target){
+          popupCall.style.display = 'none';
+        }
+      }
+    });
   };
   headerPopupCall();
+  //popup-discount
+  const popupTwo = () =>{
+    const discountBtn = document.querySelectorAll('.discount-btn'),
+      popupDiscount = document.querySelector('.popup-discount');
+
+    discountBtn.forEach((elem) =>{
+      elem.addEventListener('click', () => {
+        popupDiscount.style.display = 'block';
+      });
+    });
+    popupDiscount.addEventListener('click', () =>{
+      let target = event.target;
+      if(target.classList.contains('popup-close')){
+        popupDiscount.style.display = 'none';
+      } else {
+        target = target.closest('.popup-content');
+        if(!target){
+          popupDiscount.style.display = 'none';
+        }
+      }
+    });
+  };
+  popupTwo();
 });
